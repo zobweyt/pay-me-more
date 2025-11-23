797d2e04125005fae9a698a8646eee1cd25d55a0
GlebNes109 <testnesyutin@gmail.com>
2025-11-23 13:11:50 +0300

    feat: get resume by requestId endpoint add

--- apps/backend/src/api/resumes/routes.py
+++ apps/backend/src/api/resumes/routes.py
@@ -8,9 +8,9 @@ from src.api.tags import Tag
 router = APIRouter(prefix="/resumes", tags=[Tag.RESUMES])
 
 
-@router.get("{ResumeID}", status_code=status.HTTP_200_OK)
-async def load_resume(ResumeID: str) -> ResumeAnalyzedResponse:
-    raise NotImplementedError
+@router.get("{RequestID}", status_code=status.HTTP_200_OK, response_model=ResumeAnalyzedResponse)
+async def load_resume(RequestID: str, service: ResumeServiceDeps) -> ResumeAnalyzedResponse:
+    return await service.get_by_request_id(RequestID)
 
 
 @router.get("/skills", response_model=ResumeSkillsResponse)
