e08e0c30824099af26eb0dbba101e929b31851e6
GlebNes109 <testnesyutin@gmail.com>
2025-11-23 13:30:33 +0300

    fix: resumes/{RequestID} 500 fixed

--- apps/backend/src/api/resumes/repository.py
+++ apps/backend/src/api/resumes/repository.py
@@ -3,8 +3,12 @@ from uuid import UUID
 
 from fastapi import Depends
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
+from src.api.recommendations.schemas import RecommendationDTO
 from src.api.resumes.models import Resume, Skill
+from src.api.resumes.schemas import ResumeAnalyzedResponse
+from src.api.salary_fork.schemas import SalaryDTO
 from src.db.deps import SessionDepends
 
 
@@ -21,10 +25,43 @@ class ResumeRepository:
 
         return resume
 
-    async def get_by_request_id(self, request_id: UUID) -> Resume | None:
-        query = select(Resume).where(Resume.request_id == request_id)
-        result = await self.session.execute(query)
-        return result.scalar_one_or_none()
+    async def get_by_request_id(self, request_id: UUID) -> ResumeAnalyzedResponse | None:
+        query = (
+            select(Resume)
+            .where(Resume.request_id == request_id)
+            .options(
+                joinedload(Resume.skills),
+                joinedload(Resume.salary),
+                joinedload(Resume.recommendation),
+            )
+        )
+        resume = (await self.session.execute(query)).unique().scalar_one_or_none()
+
+        if not resume:
+            return None
+
+        salary_dto = None
+        if resume.salary is not None:
+            salary_dto = SalaryDTO(
+                from_=resume.salary.from_,  # type: ignore
+                to=resume.salary.to,
+            )
+
+        recs_dto = []
+        if resume.recommendation:
+            recs_dto = [
+                RecommendationDTO(title=r.title, subtitle=r.subtitle, result=r.result) for r in resume.recommendation
+            ]
+
+        return ResumeAnalyzedResponse(
+            request_id=resume.request_id,
+            role=resume.role,
+            experience=resume.experience,
+            location=resume.location,
+            skills=[s.name for s in resume.skills],
+            salary=salary_dto,
+            recommendations=recs_dto,
+        )
 
 
 ResumeRepositoryDeps = Annotated[ResumeRepository, Depends(ResumeRepository)]
